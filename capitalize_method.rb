4d5b6b8a709848015cb69748f46f1e2b165029d3
Mohammad.Daylaki <mohammad.daylaky@gmail.com>
2017-01-16 21:34:30 +0300

    Update capitalize_method.rb

---
+++ capitalize_method.rb
@@ -0,0 +1,28 @@
+class Person 
+  attr_accessor :name,:age,:email 
+  @@all=[]
+  def initialize(name,age,email)
+    @name=name 
+    @age=age 
+    @email=email
+    normalize_name #=> we write our method here because we need to save our name in capitalize way before it's saved inside @@all varaible .
+    @@all << self
+  end
+  def self.all 
+    @@all
+  end
+  def self.find_by_name(name)
+    @@all.find{|p| p.name==name}
+  end
+  def normalize_name #=> here also we can defind this mrthod as class method 
+    @name= name.split(" ").map{|word| word.capitalize}.join(" ")
+  end
+end
+
+shereen=Person.new("shereen messi",29,"s.m@gmail.com")
+liloz=Person.new("Liloz suliman",19,"l.s@gmail.com")
+Person.find_by_name("Liloz")
+# shereen.normalize_name 
+Person.all[0]
+
+ 
